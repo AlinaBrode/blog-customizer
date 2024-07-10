9bfc372fb8bf25d9eb7fa4ea027a19d05b769601
Alina <alinbrode@yandex.ru>
2024-07-10 18:20:43 +0300

    Select font

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -4,15 +4,27 @@ import { Button } from 'components/button';
 import styles from './ArticleParamsForm.module.scss';
 import { useState } from 'react';
 import { classNames } from 'src/utils/utils';
+import { Text } from '../text';
+import { Select } from '../select';
+import { fontFamilyOptions } from 'src/constants/articleProps';
 
 export const ArticleParamsForm = () => {
 	const [isOpen, setIsOpen] = useState(false);
 
+	const [fontSelected, setFontSelected] = useState(fontFamilyOptions[0]);
+
 	return (
 		<>
-			<ArrowButton onClick={()=>setIsOpen(true)} isOpen={isOpen}/>
+			<ArrowButton onClick={()=>setIsOpen(!isOpen)} isOpen={isOpen}/>
 			<aside className={classNames(styles.container, isOpen && styles.container_open)}>
 				<form className={styles.form}>
+					<Text as="h2" weight={800} size={31} uppercase>задайте параметры</Text>
+					<Select
+						selected={fontSelected}
+						onChange={setFontSelected}
+						options={fontFamilyOptions}
+						title='шрифт'
+					></Select>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
 						<Button title='Применить' type='submit' />
