65a91f4138d076d01d9c1673d4aeaa3634b150ac
Alina <alinbrode@yandex.ru>
2024-07-11 18:27:22 +0300

    all components are in place

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -6,12 +6,18 @@ import { useState } from 'react';
 import { classNames } from 'src/utils/utils';
 import { Text } from '../text';
 import { Select } from '../select';
-import { fontFamilyOptions } from 'src/constants/articleProps';
+import { backgroundColors, contentWidthArr, fontColors, fontFamilyOptions, fontSizeOptions } from 'src/constants/articleProps';
+import { RadioGroup } from '../radio-group';
+import { Separator } from '../separator';
 
 export const ArticleParamsForm = () => {
 	const [isOpen, setIsOpen] = useState(false);
 
 	const [fontSelected, setFontSelected] = useState(fontFamilyOptions[0]);
+	const [fontSizeSelected, setFontSizeSelected] = useState(fontSizeOptions[0]);
+	const [fontColorSelected, setFontColorSelected] = useState(fontColors[0]);
+	const [bgColorSelected, setBgColorSelected] = useState(backgroundColors[0]);
+	const [widthSelected, setWidthSelected] = useState(contentWidthArr[0]);
 
 	return (
 		<>
@@ -19,12 +25,45 @@ export const ArticleParamsForm = () => {
 			<aside className={classNames(styles.container, isOpen && styles.container_open)}>
 				<form className={styles.form}>
 					<Text as="h2" weight={800} size={31} uppercase>задайте параметры</Text>
+
 					<Select
 						selected={fontSelected}
 						onChange={setFontSelected}
 						options={fontFamilyOptions}
 						title='шрифт'
 					></Select>
+
+					<RadioGroup
+						selected={fontSizeSelected}
+						name='что-нить'
+						onChange={setFontSizeSelected}
+						options={fontSizeOptions}
+						title='размер шрифта'
+					></RadioGroup>
+
+					<Select
+						selected={fontColorSelected}
+						onChange={setFontColorSelected}
+						options={fontColors}
+						title='цвет шрифта'
+					></Select>
+
+					<Separator />
+
+					<Select
+						selected={bgColorSelected}
+						onChange={setBgColorSelected}
+						options={backgroundColors}
+						title='цвет фона'
+					></Select>
+
+					<Select
+						selected={widthSelected}
+						onChange={setWidthSelected}
+						options={contentWidthArr}
+						title='ширина контента'
+					></Select>
+
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
 						<Button title='Применить' type='submit' />
