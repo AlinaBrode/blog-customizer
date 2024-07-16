201cf3c3c97a38c6a20e400d75fb66954b5efe80
tarstars <142857@mai.ru>
2024-07-16 19:35:11 +0300

    set and reset variables

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,10 +1,9 @@
 import { ArrowButton } from 'components/arrow-button';
-import { Button } from 'components/button';
 
 import styles from './ArticleParamsForm.module.scss';
 import { ReactNode, useState } from 'react';
 import { classNames } from 'src/utils/utils';
-import { Text } from '../text';
+import { Button } from '../button';
 import { Select } from '../select';
 import { OptionType, backgroundColors, contentWidthArr, fontColors, fontFamilyOptions, fontSizeOptions } from 'src/constants/articleProps';
 import { RadioGroup } from '../radio-group';
@@ -22,16 +21,10 @@ export const ArticleParamsForm = ({children} : PropsArticleParamsForm) => {
 		<>
 			<ArrowButton onClick={()=>setIsOpen(!isOpen)} isOpen={isOpen}/>
 			<aside className={classNames(styles.container, isOpen && styles.container_open)}>
-				<form className={styles.form}>
-					<Text as="h2" weight={800} size={31} uppercase>задайте параметры</Text>
+			<form className={styles.form}>
+				{children}
 
-					{children}
-
-					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' type='reset' />
-						<Button title='Применить' type='submit' />
-					</div>
-				</form>
+			</form>
 			</aside>
 		</>
 	);
--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,19 @@ import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
+import { Button } from 'components/button';
+
 import { backgroundColors, contentWidthArr, defaultArticleState, fontColors, fontFamilyOptions, fontSizeOptions } from './constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
+import articleParamsStyles from './components/article-params-form/ArticleParamsForm.module.scss';
+
 import { Select } from './components/select';
 import { RadioGroup } from './components/radio-group';
 import { Separator } from './components/separator';
+import { Text } from './components/text';
+
 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
@@ -22,57 +28,86 @@ const App = () => {
 	const [bgColorSelected, setBgColorSelected] = useState(defaultArticleState.backgroundColor);
 	const [widthSelected, setWidthSelected] = useState(defaultArticleState.contentWidth);
 
+	const [fontSelectedView, setFontSelectedView] = useState(defaultArticleState.fontFamilyOption);
+	const [fontSizeSelectedView, setFontSizeSelectedView] = useState(defaultArticleState.fontSizeOption);
+	const [fontColorSelectedView, setFontColorSelectedView] = useState(defaultArticleState.fontColor);
+	const [bgColorSelectedView, setBgColorSelectedView] = useState(defaultArticleState.backgroundColor);
+	const [widthSelectedView, setWidthSelectedView] = useState(defaultArticleState.contentWidth);
+
+	function translateState() {
+		setFontSelectedView(fontSelected);
+		setFontSizeSelectedView(fontSizeSelected);
+		setFontColorSelectedView(fontColorSelected);
+		setBgColorSelectedView(bgColorSelected);
+		setWidthSelectedView(widthSelected);
+	}
+
+	function restoreState() {
+		setFontSelected(fontSelectedView);
+		setFontSizeSelected(fontSizeSelectedView);
+		setFontColorSelected(fontColorSelectedView);
+		setBgColorSelected(bgColorSelectedView);
+		setWidthSelected(widthSelectedView);
+	}
+
 
 	return (
 		<div
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': fontSelected.value,
-					'--font-size': fontSizeSelected.value,
-					'--font-color': fontColorSelected.value,
-					'--container-width': widthSelected.value,
-					'--bg-color': bgColorSelected.value,
+					'--font-family': fontSelectedView.value,
+					'--font-size': fontSizeSelectedView.value,
+					'--font-color': fontColorSelectedView.value,
+					'--container-width': widthSelectedView.value,
+					'--bg-color': bgColorSelectedView.value,
 				} as CSSProperties
 			}>
 			<ArticleParamsForm>
-				<Select
-						selected={fontSelected}
-						onChange={setFontSelected}
-						options={fontFamilyOptions}
-						title='шрифт'
-				></Select>
+					<Text as="h2" weight={800} size={31} uppercase>задайте параметры</Text>
 
-				<RadioGroup
-					selected={fontSizeSelected}
-					name='что-нить'
-					onChange={setFontSizeSelected}
-					options={fontSizeOptions}
-					title='размер шрифта'
-				></RadioGroup>
+					<Select
+							selected={fontSelected}
+							onChange={setFontSelected}
+							options={fontFamilyOptions}
+							title='шрифт'
+					></Select>
 
-				<Select
-					selected={fontColorSelected}
-					onChange={setFontColorSelected}
-					options={fontColors}
-					title='цвет шрифта'
-				></Select>
+					<RadioGroup
+						selected={fontSizeSelected}
+						name='fontSize'
+						onChange={setFontSizeSelected}
+						options={fontSizeOptions}
+						title='размер шрифта'
+					></RadioGroup>
 
-				<Separator />
+					<Select
+						selected={fontColorSelected}
+						onChange={setFontColorSelected}
+						options={fontColors}
+						title='цвет шрифта'
+					></Select>
 
-				<Select
-					selected={bgColorSelected}
-					onChange={setBgColorSelected}
-					options={backgroundColors}
-					title='цвет фона'
-				></Select>
+					<Separator />
 
-				<Select
-					selected={widthSelected}
-					onChange={setWidthSelected}
-					options={contentWidthArr}
-					title='ширина контента'
-				></Select>
+					<Select
+						selected={bgColorSelected}
+						onChange={setBgColorSelected}
+						options={backgroundColors}
+						title='цвет фона'
+					></Select>
+
+					<Select
+						selected={widthSelected}
+						onChange={setWidthSelected}
+						options={contentWidthArr}
+						title='ширина контента'
+					></Select>
+
+					<div className={articleParamsStyles.bottomContainer}>
+						<Button title='Сбросить' type='reset' onClick={restoreState}/>
+						<Button title='Применить' type={undefined} onClick={translateState}/>
+					</div>
 
 			</ArticleParamsForm>
 			<Article/>
